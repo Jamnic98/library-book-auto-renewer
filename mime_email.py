dfacfe968113407ac07a8a119b1401a0f5f63d8f
jamnic98 <jamiestimpson30@gmail.com>
2021-08-31 17:42:11 +0100

    added html list

--- mime_email.py
+++ mime_email.py
@@ -19,20 +19,13 @@ class MimeEmail:
         self.message.attach(MIMEText(html_content, 'html'))
 
 
-def create_table_rows(books):
-    return ''.join(create_table_row(book) for book in books)
+def create_list_item(data):
+    return wrap_element("li", data)
 
 
-def create_table_row(book):
-    style = 'border: 1px solid black; padding: 2px'
-    data = [book.title, book.author, book.due_date, book.times_renewed]
-    table_data = ''.join([wrap_element("td", data_point, style) for data_point in data])
-    return wrap_element("tr", table_data, style)
-
-
-def create_table_header(data, style):
-    header_data = ''.join([wrap_element("th", data_point) for data_point in data])
-    return wrap_element("tr", header_data, style)
+def create_list(books):
+    list_items = ''.join([create_list_item(book.title) for book in books])
+    return wrap_element('ul', list_items)
 
 
 def wrap_element(tag_name, data, style=''):
@@ -40,13 +33,7 @@ def wrap_element(tag_name, data, style=''):
 
 
 def generate_html_message(message, books):
-    style = 'border: 1px solid black; padding: 2px'
-    column_titles = ['Title', 'Author', 'Due Date', 'Times Renewed']
-    table_header = create_table_header(column_titles, style)
-    table_rows = create_table_rows(books)
-    table = wrap_element("table", f'{table_header}{table_rows}', style)
-
     return f'<html><body>' \
            f'<p>{message}</p><br/>' \
-           f'{table}' \
+           f'{create_list(books)}' \
            f'</body></html> '
